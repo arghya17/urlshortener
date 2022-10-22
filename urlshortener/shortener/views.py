253c08eeab8a57c9ee9d5014bc3c8a44e0ef4b75
arghya <arghyadeepsaha17@gmail.com-->
2022-10-22 10:45:56 +0530

    template done

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -18,13 +18,20 @@ def task_view(request):
     }
     return render(request, 'task.html', data)
 
+
 def shorten_url(request):
+    context = {
+        "submitted": False,
+    }
     if request.method == "POST":
+        context['submitted'] = True
         user_data = request.POST
 
         long_url = user_data['longurl']
         custom_name = user_data['custom_name']
-
+        data = {}
+        data['long_url'] = long_url
+        data['short_url'] = request.build_absolute_uri() + custom_name
+        context['data'] = data
         print(long_url, custom_name)
-
-    return render(request, 'home.html')
+    return render(request, 'home.html', context)
--- urlshortener/urlshortener/urls.py
+++ urlshortener/urlshortener/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('url/',include('shortener.urls')),
+    path('', include('shortener.urls')),
 ]
